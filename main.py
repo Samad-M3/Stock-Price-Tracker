a0eeae78e361fe365d67cb56bdce06bb204c34ca
Samad <abdus-samad_mohit@hotmail.com>
2025-09-12 18:41:55 +0100

    Checkpoint before rewriting historical data handling

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from datetime import datetime
 
 COLUMN_NAMES = ["Date", "Open", "High", "Low", "Close", "Volume", "Ticker"]
 
-def fetch_historical_data(list_of_tickers, start, end, interval="1d"):
+def fetch_historical_data(list_of_tickers, start, end, interval):
     start = pd.to_datetime(start).tz_localize("America/New_York")
     end = pd.to_datetime(end).tz_localize("America/New_York")
+    filename = get_filename(interval)
 
     if Path("historical_data.csv").exists():
         compiled_history = load_from_csv("historical_data.csv")
@@ -32,20 +33,29 @@ def fetch_historical_data(list_of_tickers, start, end, interval="1d"):
             else:
                 if start < ticker_specific_dataframe["Date"].min():
                     ticker_object = yf.Ticker(ticker)
-                    history = ticker_object.history(start=start, end=ticker_specific_dataframe["Date"].min(), interval=interval).reset_index().drop(["Dividends", "Stock Splits"], axis="columns", errors="ignore")
+                    history = ticker_object.history(start=start, end=ticker_specific_dataframe["Date"].min(), interval=interval).reset_index().drop(["Dividends", "Stock Splits", "Adj Close"], axis="columns", errors="ignore")
                     history["Ticker"] = ticker
-                    compiled_history = pd.concat([compiled_history, history], ignore_index=True)
+
+                    if history.empty:
+                        pass
+                    else:
+                        compiled_history = pd.concat([compiled_history, history], ignore_index=True)
+
                 if end > ticker_specific_dataframe["Date"].max():
                     ticker_object = yf.Ticker(ticker)
-                    history = ticker_object.history(start=ticker_specific_dataframe["Date"].max(), end=end, interval=interval).reset_index().drop(["Dividends", "Stock Splits"], axis="columns", errors="ignore")
+                    history = ticker_object.history(start=ticker_specific_dataframe["Date"].max(), end=end, interval=interval).reset_index().drop(["Dividends", "Stock Splits", "Adj Close"], axis="columns", errors="ignore")
                     history["Ticker"] = ticker
-                    compiled_history = pd.concat([compiled_history, history], ignore_index=True)
+
+                    if history.empty:
+                        pass
+                    else:
+                        compiled_history = pd.concat([compiled_history, history], ignore_index=True)
 
             fully_checked_tickers.append(ticker)
                 
         for ticker in missing_tickers:
             ticker_object = yf.Ticker(ticker)
-            history = ticker_object.history(start=start, end=end, interval=interval).reset_index().drop(["Dividends", "Stock Splits"], axis="columns", errors="ignore")
+            history = ticker_object.history(start=start, end=end, interval=interval).reset_index().drop(["Dividends", "Stock Splits", "Adj Close"], axis="columns", errors="ignore")
             history["Ticker"] = ticker
             compiled_history = pd.concat([compiled_history, history], ignore_index=True)
             fully_checked_tickers.append(ticker)   
@@ -53,7 +63,6 @@ def fetch_historical_data(list_of_tickers, start, end, interval="1d"):
         compiled_history.drop_duplicates(subset=["Date", "Ticker"], inplace=True)
         save_to_csv(compiled_history, "historical_data.csv")    
         
-        print("Loading data from CSV...")
         combined_resulting_dataframe = pd.DataFrame(columns=COLUMN_NAMES) # Creating an empty dataframe so each tickers filtered data can be appeneded on and representred as one big dataframe
         # Forces each column into the correct data type
         combined_resulting_dataframe = combined_resulting_dataframe.astype({
@@ -69,7 +78,7 @@ def fetch_historical_data(list_of_tickers, start, end, interval="1d"):
         for ticker in fully_checked_tickers:
             combined_resulting_dataframe = pd.concat([combined_resulting_dataframe, compiled_history[(compiled_history["Ticker"] == ticker) & (compiled_history["Date"] >= start) & (compiled_history["Date"] <= end)]])
 
-        print(combined_resulting_dataframe)
+        print(combined_resulting_dataframe.to_string())
     else:
         print("Fetching data from API...")
         compiled_history = pd.DataFrame(columns=COLUMN_NAMES)
@@ -86,7 +95,7 @@ def fetch_historical_data(list_of_tickers, start, end, interval="1d"):
 
         for single_ticker in list_of_tickers:
             ticker = yf.Ticker(single_ticker)
-            history = ticker.history(start=start, end=end, interval=interval).reset_index().drop(["Dividends", "Stock Splits"], axis="columns", errors="ignore")
+            history = ticker.history(start=start, end=end, interval=interval).reset_index().drop(["Dividends", "Stock Splits", "Adj Close"], axis="columns", errors="ignore")
             history["Ticker"] = single_ticker
             compiled_history = pd.concat([compiled_history, history], ignore_index=True)
 
@@ -108,12 +117,12 @@ def load_from_csv(filename):
     else:
         print("File does not exist")
 
+def get_filename(interval):
+    return f"data/historical_data_{interval}.csv"
+
 # fetch_historical_data(["AAPL", "AMZN", "MSFT"], "2025-08-01", "2025-09-01", "1d")
-
 # fetch_historical_data(["MSFT", "TSLA"], "2025-08-10", "2025-08-23", "1d")
-
 # fetch_historical_data(["MSFT", "AMZN", "META", "GOOGL"], "2025-09-01", "2025-09-10", "1d")
-
-fetch_historical_data(["NFLX", "NVDA", "AAPL"], "2025-07-01", "2025-07-10", "1d")
+# fetch_historical_data(["AAPL", "MSFT"], "2025-06-10", "2025-06-24", "1d")
 
 # fetch_live_price(["AAPL", "MSFT", "TSLA", "AMZN"])
